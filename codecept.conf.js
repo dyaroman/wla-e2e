533f96aa89d524734af3c2a1c23a96dece52530a
Roman Diachenko <dyaroman@gmail.com>
2022-05-18 09:34:44 +0300

    WIP

---
+++ codecept.conf.js
@@ -0,0 +1,27 @@
+const { setHeadlessWhen, setCommonPlugins } = require('@codeceptjs/configure');
+
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+// enable all common plugins https://github.com/codeceptjs/configure#setcommonplugins
+setCommonPlugins();
+
+exports.config = {
+  tests: './test/*.test.js',
+  output: './output',
+  helpers: {
+    Playwright: {
+      url: 'http://localhost',
+      show: true,
+      browser: 'chromium',
+      windowSize: '1280x720'
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  bootstrap: null,
+  mocha: {},
+  name: 'wla-tests'
+}
