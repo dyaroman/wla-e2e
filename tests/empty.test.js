2b6fd8f73eec3814dd49a76024300fb69e2127bb
Roman Diachenko <dyaroman@gmail.com>
2024-04-16 11:26:37 +0300

    actualize 'no data' test

--- tests/empty.test.js
+++ tests/empty.test.js
@@ -3,16 +3,7 @@ const { URL } = require('../misc/config');
 Feature('no data to show @static @sms');
 
 Scenario('not existing website', async ({ I }) => {
-  I.amOnPage(URL);
-  I.waitForElement('table', 60);
-  const filtersCollapse = await I.grabAttributeFrom(
-    'details.filters',
-    'open',
-  ).then((attr) => attr === null);
-  if (filtersCollapse) {
-    I.click('details.filters summary');
-  }
-  I.fillField('[data-qa="website"]', 'google.com');
+  I.amOnPage(`${URL}/?website=google.com`);
   I.seeTextEquals('Websites: 0', '[data-qa="websitesNumber"]');
   I.seeInTitle('[0]');
   I.seeTextEquals(
