891a358243f69480dd4314c1e74f2fd5a5ea7cac
Roman Diachenko <dyaroman@gmail.com>
2024-04-16 16:36:49 +0300

    add quick search test

---
+++ tests/quick-search.test.js
@@ -0,0 +1,25 @@
+const { URL, DATA_URL } = require('../misc/config');
+const { WEBSITES_DATA } = require('../misc/consts');
+const { getRandomNumber } = require('../misc/functions');
+
+Feature('quick search @static @sms');
+
+Scenario('alt + click table cell', async ({ I }) => {
+  const response = await I.makeApiRequest(
+    'GET',
+    `${DATA_URL}/${WEBSITES_DATA}`,
+    {},
+  );
+  const { websites } = await response['json']();
+  const randomNumber = getRandomNumber(0, websites.length - 1);
+  const websiteData = websites[randomNumber - 1];
+
+  I.amOnPage(`${URL}/?showColumns=website`);
+  I.waitForElement('table', 60);
+  I.click(`.table tr:nth-child(${randomNumber}) [data-qa='website']`, null, {
+    modifiers: ['Alt'],
+  });
+  I.seeInTitle(`[1]`);
+  I.seeInCurrentUrl(`website=${websiteData['website']}`);
+  I.seeInField('input[data-qa="website"]', websiteData['website']);
+});
