1c6474a55ddee83c61295a26b5f6421f265ca532
Roman Diachenko <dyaroman@gmail.com>
2024-04-16 17:29:25 +0300

    disabled tags

---
+++ tests/disabled-tags.test.js
@@ -0,0 +1,24 @@
+const { URL } = require('../misc/config');
+
+Feature('disabled tags @static @sms');
+
+Scenario('should see disabled tags if select "uk" tag', async ({ I }) => {
+  I.amOnPage(`${URL}/?filtersOpen=`);
+  I.waitForElement('table', 60);
+  const disabledTagsBefore = await I.grabNumberOfVisibleElements(
+    '.filters .checkbox.disabled',
+  );
+  if (disabledTagsBefore > 0) {
+    throw new Error(`Disabled tags must be 0, but got ${disabledTagsBefore}`);
+  }
+  I.click(`.filters .checkbox[data-qa='uk']`);
+  const disabledTagsAfter = await I.grabNumberOfVisibleElements(
+    '.filters .checkbox.disabled',
+  );
+  if (disabledTagsAfter === 0) {
+    throw new Error(
+      `Disabled tags must be more than 0, but got ${disabledTagsAfter}`,
+    );
+  }
+  console.log({ disabledTagsBefore, disabledTagsAfter });
+});
