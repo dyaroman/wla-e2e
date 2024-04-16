9de589d86d6ac8e5a7ce2eebb2420f3e5de2e8ed
Roman Diachenko <dyaroman@gmail.com>
2024-04-16 15:26:28 +0300

    add check gtm key test

---
+++ tests/gtm.test.js
@@ -0,0 +1,10 @@
+const { URL } = require('../misc/config');
+
+Feature('gtm @static @sms');
+
+Scenario('check key', async ({ I }) => {
+  I.amOnPage(URL);
+  I.seeInSource('https://www.googletagmanager.com/gtm.js');
+  I.seeInSource('GTM-XXXXXXXX');
+  I.seeInSource('https://www.googletagmanager.com/ns.html?id=GTM-XXXXXXXX');
+});
