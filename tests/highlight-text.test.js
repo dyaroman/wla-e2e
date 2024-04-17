90afd259deff3d84b33a0ceca37ad430f175d500
Roman Diachenko <dyaroman@gmail.com>
2024-04-17 15:56:16 +0300

    add highlight text test

---
+++ tests/highlight-text.test.js
@@ -0,0 +1,9 @@
+const { URL } = require('../misc/config');
+
+Feature('highlight');
+
+Scenario('should highlight search matched text', async ({ I }) => {
+  I.amOnPage(`${URL}/?website=loan`);
+  I.waitForElement('table', 60);
+  I.seeElement('table mark');
+});
