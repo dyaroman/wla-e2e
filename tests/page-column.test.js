467e199570d822dbd99c4fa8c069f876a5326863
Roman Diachenko <dyaroman@gmail.com>
2024-11-08 10:56:40 +0200

    actualize page-column.test.js

--- tests/page-column.test.js
+++ tests/page-column.test.js
@@ -8,12 +8,12 @@ Scenario('should filter websites by "==sc" page', async ({ I }) => {
   I.waitForElement('table', 60);
   const websitesNumberByPageFilter =
     (await I.grabNumberOfVisibleElements('tr')) - 1;
-  I.amOnPage(`${URL}/?tags=ocs`);
+  I.amOnPage(`${URL}/?tags=(f) enableOCS`);
   const websitesNumberByOcsTag =
     (await I.grabNumberOfVisibleElements('tr')) - 1;
   if (websitesNumberByPageFilter !== websitesNumberByOcsTag) {
     throw new Error(
-      `Number of websites filtered by page '==sc' (${websitesNumberByPageFilter}) and filtered by 'ocs' tag (${websitesNumberByOcsTag}) should be equal!`,
+      `Number of websites filtered by page '==sc' (${websitesNumberByPageFilter}) and filtered by 'enableOCS' tag (${websitesNumberByOcsTag}) should be equal!`,
     );
   }
 });
