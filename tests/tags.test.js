63abf0848c54441a1c804b96343ea56df028f0da
Roman Diachenko <dyaroman@gmail.com>
2025-03-20 12:35:33 +0200

    actualize 'tags' test

--- tests/tags.test.js
+++ tests/tags.test.js
@@ -3,7 +3,7 @@ const { SIDEBAR_OPEN } = require('../misc/consts');
 
 Feature('tags');
 
-Scenario('should see disabled tags if select "uk" tag', async ({ I }) => {
+Scenario('should see disabled tags if select "es" tag', async ({ I }) => {
   I.amOnPage(`${URL}/?${SIDEBAR_OPEN}=`);
   I.waitForElement('table', 60);
   const disabledTagsBefore = await I.grabNumberOfVisibleElements(
@@ -12,7 +12,7 @@ Scenario('should see disabled tags if select "uk" tag', async ({ I }) => {
   if (disabledTagsBefore > 0) {
     throw new Error(`Disabled tags must be 0, but got ${disabledTagsBefore}`);
   }
-  I.click(`.tags label[data-qa='uk']`);
+  I.click(`.tags label[data-qa='es']`);
   const disabledTagsAfter = await I.grabNumberOfVisibleElements(
     '.tags label.disabled',
   );
@@ -24,33 +24,33 @@ Scenario('should see disabled tags if select "uk" tag', async ({ I }) => {
   console.log({ disabledTagsBefore, disabledTagsAfter });
 });
 
-Scenario('should exclude websites that have the "uk" tag', async ({ I }) => {
+Scenario('should exclude websites that have the "es" tag', async ({ I }) => {
   I.amOnPage(`${URL}/?${SIDEBAR_OPEN}=`);
   I.waitForElement('table', 60);
   const numberOfAllWebsites = await I.grabTextFrom('[data-qa="counter"]').then(
     (str) => Number(str),
   );
   // include
-  I.click(`.tags label[data-qa='uk']`);
-  const numberOfIncludeUkWebsites = await I.grabTextFrom(
+  I.click(`.tags label[data-qa='es']`);
+  const numberOfIncludeWebsites = await I.grabTextFrom(
     '[data-qa="counter"]',
   ).then((str) => Number(str));
   // exclude
-  I.click(`.tags label[data-qa='uk']`);
-  const numberOfExcludeUkWebsites = await I.grabTextFrom(
+  I.click(`.tags label[data-qa='es']`);
+  const numberOfExcludeWebsites = await I.grabTextFrom(
     '[data-qa="counter"]',
   ).then((str) => Number(str));
   console.log({
     numberOfAllWebsites,
-    numberOfIncludeUkWebsites,
-    numberOfExcludeUkWebsites,
+    numberOfIncludeWebsites,
+    numberOfExcludeWebsites,
   });
   if (
     numberOfAllWebsites !==
-    numberOfIncludeUkWebsites + numberOfExcludeUkWebsites
+    numberOfIncludeWebsites + numberOfExcludeWebsites
   ) {
     throw new Error(
-      'Number of all websites should be equal to sum of UK and non UK websites',
+      'Number of all websites should be equal to sum of include and exclude websites',
     );
   }
 });
