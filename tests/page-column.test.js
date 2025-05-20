a2eb8eddeefa9ca7b74221cdf5acadde7324dd18
Roman Diachenko <dyaroman@gmail.com>
2025-05-20 15:49:54 +0300

    refactoring getting table rows

--- tests/page-column.test.js
+++ tests/page-column.test.js
@@ -7,10 +7,10 @@ Scenario('should filter websites by "==sc" page', async ({ I }) => {
   I.amOnPage(`${URL}/?pages===sc&${SHOW_COLUMNS}=website,pages`);
   I.waitForElement('table', 60);
   const websitesNumberByPageFilter =
-    (await I.grabNumberOfVisibleElements('tr')) - 1;
+    await I.grabNumberOfVisibleElements('tbody tr');
   I.amOnPage(`${URL}/?tags=(f) enableOCS`);
   const websitesNumberByOcsTag =
-    (await I.grabNumberOfVisibleElements('tr')) - 1;
+    await I.grabNumberOfVisibleElements('tbody tr');
   if (websitesNumberByPageFilter !== websitesNumberByOcsTag) {
     throw new Error(
       `Number of websites filtered by page '==sc' (${websitesNumberByPageFilter}) and filtered by 'enableOCS' tag (${websitesNumberByOcsTag}) should be equal!`,
@@ -20,7 +20,7 @@ Scenario('should filter websites by "==sc" page', async ({ I }) => {
 
 Scenario('should show none websites by "!=index" page', async ({ I }) => {
   I.amOnPage(`${URL}/?pages=!=index`);
-  const websitesNumber = await I.grabNumberOfVisibleElements('tr');
+  const websitesNumber = await I.grabNumberOfVisibleElements('tbody tr');
   if (websitesNumber !== 0) {
     throw new Error(
       `Number of websites filtered by page '!=index' should be 0, but got ${websitesNumber}!`,
